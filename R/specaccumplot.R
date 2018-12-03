21d358e6eb4e047b41e74c63d78e0e94a9f75b7e
Paul Efren Santos Andrade <paulefrens@gmail.com>
2018-12-04 08:50:13 +1100

    new functions

---
+++ R/specaccumplot.R
@@ -0,0 +1,50 @@
+
+#' specaccumplot
+#'
+#' @param comm
+#' @param method Species accumulation method (partial match).  Use the same options as specaccum function from vegan package, "random" adds sites in random order, "exact" finds the expected (mean) species richness, "coleman" finds the expected richness, and "rarefaction" finds the mean when accumulating individuals instead of sites. Except for "collector" method.
+#'
+#' @return
+#' @export
+#' @seealso specaccum
+#' @importFrom vegan decostand
+#' @importFrom vegan specaccum
+#' @importFrom magrittr %>%
+#' @examples
+#'
+#' require(vegan)
+#' data(dune)
+#' specaccumplot(dune)
+#'
+specaccumplot <- function(comm, method = "exact") {
+
+  df <- vegan::specaccum(comm, method = method)
+dplyr::data_frame(parcela = df$sites,
+                    riqueza = df$richness,
+                    des_sta = df$sd) %>%
+  ggplot2::ggplot(aes(parcela, riqueza))+
+  ggplot2::geom_line(linetype = 1, size = .5, color = "red") +
+  ggplot2::geom_errorbar(aes(ymin = riqueza - des_sta, ymax = riqueza + des_sta),
+                width = 0, size = 1)+
+  ggplot2::geom_point(color = "red", size = 1.5)+
+  ggplot2::theme_bw()+
+  ggplot2::theme(axis.text.x  = element_text(angle = 0,
+                                    vjust = 0.5, size = 14),
+        axis.text.y.left  = element_text(angle = 0,
+                                         vjust = 0.5, size = 14),
+        axis.title.y = element_text(size = 18),
+        axis.title.x = element_text(size = 18),
+        legend.background = element_blank(),
+        legend.key = element_blank(),
+        legend.title = element_text(size=12, face="bold"),
+        plot.title = element_text(hjust = .5,
+                                  face = "bold", size = 21),
+        legend.justification=c(0,1),
+        legend.position=c(.8, 1))+
+  ggplot2::labs( color = "",
+        fill="",
+        x = "Sites",
+        y = paste(method))
+}
+
+
